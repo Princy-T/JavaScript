4daaf19c5ebe426164c5d57e12e678099774005f
Princy-T <96741606+Princy-T@users.noreply.github.com>
2022-01-30 23:21:01 +0530

    Add files via upload

---
+++ Task-6 (Collapsible)/script.js
@@ -0,0 +1,18 @@
+const accordionTitle = document.querySelectorAll(".accordion-title");
+accordionTitle.forEach((accordionTitle) =>
+{
+  accordionTitle.addEventListener('click', () => 
+  {
+    const height = accordionTitle.nextElementSibling.scrollHeight;
+    console.log(height);
+    accordionTitle.classList.toggle("active-header");
+    if (accordionTitle.classList.contains('active-header'))
+    {
+      accordionTitle.nextElementSibling.style.maxHeight = `${height}px`; 
+    }
+    else
+    {
+      accordionTitle.nextElementSibling.style.maxHeight = "0px"; 
+    }
+  });
+});
